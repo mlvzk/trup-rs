4311ae05b5fe872a6043ab9b9aa0c640e37aae75
mlvzk <mlvzk@protonmail.com>
2021-08-29 02:43:52 +0200

    cache test

--- src/main.rs
+++ src/main.rs
@@ -137,7 +137,7 @@ impl TypeMapKey for FrameworkKey {
 async fn main() {
     init_logger();
 
-    println!("test cache5");
+    println!("test cache6");
 
     let honeycomb_api_key = std::env::var("HONEYCOMB_API_KEY").ok();
 
